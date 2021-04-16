b43129d0a363fb122529b3397dc30e7f56b429c2
Kerstin Beckersjuergen <kerstin.beckersjuergen@sap.com>
2021-04-16 15:56:10 +0200

    created classes

---
+++ src/main/java/model/implementation/Employee.java
@@ -0,0 +1,15 @@
+package model.implementation;
+
+import model.People;
+
+public class Employee extends People {
+    private int employeeID;
+    private String Name;
+
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+
+}
---
+++ src/main/java/model/implementation/MedPlan.java
@@ -0,0 +1,4 @@
+package model.implementation;
+
+public class MedPlan {
+}
