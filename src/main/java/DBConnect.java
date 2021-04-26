d2aae6330945b1e1e6fd5b4557be9bd5dd3f11e1
JakobGassauer <jakob.gassauer@studmail.hwg-lu.de>
2021-04-26 12:01:21 +0200

    created database, database function, updated in pom.xml

--- src/main/java/DBConnect.java
+++ src/main/java/DBConnect.java
@@ -1,6 +1,26 @@
 import java.sql.*;
 
-public class DBConnect {
 
+public class DBConnect {
+    public static final String location="jdbc:sqlite:src/database/group1_database.sql";
+
+    public static Connection connect() {
+
+        Connection conn = null;
+
+        try {
+            conn = DriverManager.getConnection(location);
+            System.out.println("Connection to Database established");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return conn;
+    }
+
+    public static void main(String[] args) {
+        Connection conn = connect();
+
+
+    }
 
 }
